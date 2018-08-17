ae8ee283f13c28cc6d6d2ff0543a379ee27a497b
Sharmaine Requerme <srqrme@gmail.com>
2018-08-17 01:55:44 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,13 @@
 require 'pry'
 
 class Song
+
   attr_accessor :name
   attr_reader :artist
+
+  extend Memorable::ClassMethods
+  extend Findable::ClassMethods
+
 
   @@songs = []
 
@@ -10,25 +15,17 @@ class Song
     @@songs << self
   end
 
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
+  # def self.find_by_name(name)
+    # @@songs.detect{|a| a.name == name}
+  # end
 
   def artist=(artist)
     @artist = artist
   end
 
-  def self.reset_all
-    @@songs.clear
-  end
-
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
-
-  def self.count
-    self.all.count
-  end
+  # def to_param
+    # name.downcase.gsub(' ', '-')
+  # end
 
   def self.all
     @@songs
